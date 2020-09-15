d52bc39cfcd2293908eca22ad20d45b55fb09be8
Rahul Nanda <rahul.nanda@GCCYJQ0BJ1WK.local>
2020-09-15 17:28:35 -0400

    test

---
+++ Demo_Programs/example1.py
@@ -0,0 +1,5 @@
+i = 1
+for i in range(1,13):
+	for j in range(1,13):
+		print("%5d" %(i * j))
+	print()
---
+++ Demo_Programs/example2.py
@@ -0,0 +1,10 @@
+numList = [1,3,5,7,9,11]
+colorList = ["red", "blue", "green", "orange"]
+colorList.append("yellow")
+colorList.insert(0,"purple")
+
+for n in numList:
+	print(n)
+
+for c in colorList:
+	print(c)
