37c3b719f474b33c855163180d4fec468104702b
Rahul Nanda <rahul.nanda@GCCYJQ0BJ1WK.local>
2020-09-17 14:29:38 -0400

    updated css

--- Demo_Programs/script.js
+++ Demo_Programs/script.js
@@ -1,61 +1,99 @@
 
 
 //This is a stub for improving the code by using an array
-items = document.getElementsByClassName('valueItem');
+
 
 
 //What would chanage below if the items array existed?
-startValue = Math.trunc(Math.random() * 10) + 1;
-
-current = document.getElementById('item1');
-
-current.innerHTML = startValue;
 
 missValue = Math.trunc(Math.random() * 5) + 2;
 
-operator = Math.trunc(Math.random() * 2);
-function run(){
-nextValue = startValue;
+function add() {
 
 
+    startValue = Math.trunc(Math.random() * 10) + 1;
 
-if (operator == 0) 
-{
+    item1.innerHTML = startValue;
+
+ 
     step = Math.trunc(Math.random() * 25) + 1;
+
+    console.log(step)
+
     for (i = 2; i < 7; i++) 
     {
-        nextValue = nextValue + step;
+        startValue = startValue + step;
+
+        val = (document.getElementById('item' + String(i)))
+
         if (i != missValue) 
         {
-            (document.getElementById('item' + String(i))).innerHTML = nextValue;
+            val.innerHTML = startValue;
+            console.log(startValue)
         }
         else
         {
-            answer = nextValue;
-            (document.getElementById('item' + String(i))).addEventListener('click', function()
+            answer = startValue;
+            val.addEventListener('click', function()
             {
                 response = prompt("Enter the missing value");
                 if (answer == response)
                 {
                     alert("Congratulations - Correct");
                     (document.getElementById('item' + String(missValue))).innerHTML = response;
+
+                    location.reload();
+
                 }
                 else
-                    alert("Sorry, that is not correct.  Feel free to try again");                
+                    alert("Sorry, that is not correct.  Feel free to try again");     
+                    location.reload();           
             });
         }
     }
+
+
+
 }
-else 
-{
-    step = Math.trunc(Math.random() * 3) + 2;
-        for (i = 2; i < 7; i++) 
+
+function multiply(){
+
+  startValue = Math.trunc(Math.random() * 10) + 1;
+
+    item1.innerHTML = startValue;
+
+ 
+    step = Math.trunc(Math.random() * 5) + 1;
+    console.log(step)
+    for (i = 2; i < 7; i++) 
     {
-        nextValue = nextValue * step;
+        startValue = startValue * step;
+
+        val = (document.getElementById('item' + String(i)))
+
         if (i != missValue) 
         {
-            (document.getElementById('item' + String(i))).innerHTML = nextValue;
+            val.innerHTML = startValue;
+        }
+        else
+        {
+            answer = startValue;
+            val.addEventListener('click', function()
+            {
+                response = prompt("Enter the missing value");
+                if (answer == response)
+                {
+                    alert("Congratulations - Correct");
+                    (document.getElementById('item' + String(missValue))).innerHTML = response;
+
+                    location.reload();
+                }
+                else
+                    alert("Sorry, that is not correct.  Feel free to try again");     
+
+                    location.reload();           
+            });
         }
     }
-}
+
 }
